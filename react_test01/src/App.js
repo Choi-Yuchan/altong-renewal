8a9319a1b275ddc486ec61c368056ffed4bb8d31
askePhoenix <aistarteverything@gmail.com>
2021-03-26 01:42:33 +0000

    댓글에 대한 json 처리4  #4

--- react_test01/src/App.js
+++ react_test01/src/App.js
@@ -1,9 +1,19 @@
 import QuestionBox from './components/questionBox/QuestionBox'
 
 const SSRJSON = [
+{ 
+  head: 
+  {
+    profile:"/test_source/10010006.png", locale:"KOR", thankAlmoney:"100", uLv:"1", nick: "똑똑똑", title: "지방간에 좋은 음식은 어떤 음식이 있을까요",
+    persent:"100", date:"2021-03-22 10:23:47 UTC+9", readCount: "8", 
+  },
+  contents: "지방간에 좋은 음식은 어떤 음식이 있을까요",
+  bottom:
   {
     
-  },
+  }
+
+},
 ]
 
 function App() {
