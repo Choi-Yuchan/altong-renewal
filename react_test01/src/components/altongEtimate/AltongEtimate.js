0ce7563d617be57ac49009118d31ff79bcd08f31
askePhoenix <aistarteverything@gmail.com>
2021-03-29 03:09:44 +0000

    json 로그인 유저 기능 추가 5  #7

---
+++ react_test01/src/components/altongEtimate/AltongEtimate.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import React, { useState } from 'react';
+
+const MainUl = styled.ul`
+    text-align: center;
+    width: 100%;
+    background: #f7f7f7;
+    border-radius: 15px;
+    border: 1px solid #f5f5f5;
+    padding: 5px 0;
+    margin-bottom: 10px;
+`;
+
+
+// USER={props.USER}
+
+function AltongEtimate(props) {
+
+  return (
+    <MainUl>
+        
+    </MainUl>
+  );
+}
+
+export default AltongEtimate;
---
+++ react_test01/src/components/altongEtimate/etimate/Etimate.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import React, { useState } from 'react';
+
+const MainLi = styled.li`
+    display: inline-block;
+    width: 45px;
+    margin: 0 20px;
+    position: relative;
+    list-style: none;
+`;
+const EtimateA = styled.a`
+    display: block;
+    width: 100%;
+    text-decoration: none;
+    color: #333;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+`;
+const EtimateImg = styled.img`
+    display: block;
+    width: 100%;
+    margin-bottom: 3px;
+`;
+const EtimateSpan = styled.span`
+    display: block;
+    width: 100%;
+    font-size: 12px;
+    color: #9d9d9d;
+    letter-spacing: -1px;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+`;
+
+const EtimateEm = styled.em`
+    font-style: normal;
+    color: #fd0031;
+    font-weight: bold;
+    margin-left: 3px;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+`;
+
+function Etimate(props) {
+
+  return (
+    <MainLi>
+        <EtimateA>
+            <EtimateImg></EtimateImg>
+            <EtimateSpan>알통<EtimateEm>1</EtimateEm></EtimateSpan>
+        </EtimateA>
+    </MainLi>
+  );
+}
+
+export default Etimate;
