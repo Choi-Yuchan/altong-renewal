8e6de7f26c1205483ccc72d44232bf2a907dc00b
askePhoenix <aistarteverything@gmail.com>
2021-04-16 17:21:31 +0900

    꼭대기 작업 1

--- react_test01/src/components/questionPopup/QuestionPopup.js
+++ react_test01/src/components/questionPopup/QuestionPopup.js
@@ -44,8 +44,18 @@ const MainLi2 = styled.li`
 `;
 // 찜
 function ZzimAxios(pageSeq){
-    console.log("pageSeq : " + pageSeq);
-    axios.post("/rest/questions/"+pageSeq+"/Zzim")
+    axios.put("/rest/questions/"+pageSeq+"/Zzim")
+    .then((response) => response.data)
+    .then( (data) => {
+        alert(data.message);
+    })
+    .catch(function (error) {
+        console.log("실패 : "+error);
+    });
+}
+// 꼭대기
+function moveTopAxios(pageSeq){
+    axios.post("/restApi/questions/"+pageSeq+"/movetop")
     .then((response) => response.data)
     .then( (data) => {
         alert(data.message);
@@ -77,7 +87,11 @@ function QuestionPopup(props) {
                 </Popup>
             </MainLi2>
             <MainLi>
-                <Popup text="꼭대기" imgurl="/pub/answer/answerList/images/atm_more_4.png" >
+                <Popup onClick={() => {
+                    if (confirm('이 질문을 목록 최상단으로 보내시겠습니까?\n회원님의 잔액에서 10000 알이 차감됩니다.\n(24시간 후 원래 위치로 되돌아갑니다)')){
+                        moveTopAxios(props.pageSeq);
+                    }
+                }} text="꼭대기" imgurl="/pub/answer/answerList/images/atm_more_4.png" >
                 </Popup>
             </MainLi>
             <MainLi onClick={(e) => {
