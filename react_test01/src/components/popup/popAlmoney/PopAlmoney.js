faf836b0a9f2cb37c818a13c96019e984b4abe2f
askePhoenix <aistarteverything@gmail.com>
2021-04-20 20:43:56 +0900

    work siren 15

--- react_test01/src/components/popup/popAlmoney/PopAlmoney.js
+++ react_test01/src/components/popup/popAlmoney/PopAlmoney.js
@@ -4,106 +4,6 @@ import axios from 'axios';
 
 import Num3Comma from '../../functions/num3comma/Num3Comma'
 
-const PopAlDiv = styled.div`
-    display: ${(props) => props.showAlmoney?"block":"none"};
-    padding: 10px;
-    max-width: 320px;
-    text-align: center;
-    letter-spacing: -1px;
-    background: #fff;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border-radius: 20px;
-    overflow: hidden;
-    width: 80%;
-    z-index:199;
-`;
-const PopUl = styled.ul`
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    margin:0;
-    padding:0;
-`;
-const Popli = styled.li`
-    list-style: none;
-`;
-const PopP = styled.p`
-    margin-top: 10px;
-    font-weight: 500;
-    font-size: 14px;
-    padding: 0;
-    box-sizing: border-box;
-    -webkit-tap-highlight-color: transparent;
-`;
-const PopSpan = styled.span`
-    font-weight: bold;
-    color: #fd0031;
-`;
-const Popli3 = styled.li`
-    font-weight: bold;
-    margin: 20px 0;
-    list-style: none;
-`;
-const PopH3 = styled.h3`
-    font-size: 16px;
-    line-height: 50px;
-    border-bottom: 1px solid #eee;
-`;
-const PopH3Input = styled.input`
-    width: 100px;
-    font-size: 14px;
-    border: none;
-    border-bottom: 1px solid #333;
-    margin-right: 10px;
-    padding: 6px 0;
-    font-weight: bold;
-    text-align: center;
-    outline: none;
-`;
-const Popli4 = styled.li`
-    display: inline-block;
-    width: 50%;
-    margin-left: -2px;
-    list-style: none;
-`;
-const Popli4Button = styled.button`
-    margin-right: 15px;
-    border: 1px solid #333;
-    color: #666;
-    margin-top: 10px;
-    margin-bottom: 5px;
-    width: 43%;
-    background: #fff;
-    font-size: 14px;
-    font-weight: bold;
-    border-radius: 20px;
-    line-height: 25px;
-    outline: none;
-    cursor: pointer;
-`;
-const Popli5 = styled.li`
-    display: inline-block;
-    width: 50%;
-    margin-left: -2px;
-    list-style: none;
-`;
-const Popli5Button = styled.button`
-    margin-top: 10px;
-    margin-bottom: 5px;
-    width: 43%;
-    border: 1px solid #fd0031;
-    background: #fff;
-    font-size: 14px;
-    font-weight: bold;
-    border-radius: 20px;
-    line-height: 25px;
-    color: #fd0031;
-    outline: none;
-    cursor: pointer;
-`;
 
 function giveAlmoney(props, extraAlmoney, setMaxAlmoney, setextraAlmoney, maxAlmoney) {
         
@@ -225,5 +125,105 @@ function PopAlmoney(props) {
     );
 }
   
-  export default PopAlmoney;
-  
+export default PopAlmoney;
+
+const PopAlDiv = styled.div`
+    display: ${(props) => props.showAlmoney?"block":"none"};
+    padding: 10px;
+    max-width: 320px;
+    text-align: center;
+    letter-spacing: -1px;
+    background: #fff;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 20px;
+    overflow: hidden;
+    width: 80%;
+    z-index:199;
+`;
+const PopUl = styled.ul`
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    margin:0;
+    padding:0;
+`;
+const Popli = styled.li`
+    list-style: none;
+`;
+const PopP = styled.p`
+    margin-top: 10px;
+    font-weight: 500;
+    font-size: 14px;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+`;
+const PopSpan = styled.span`
+    font-weight: bold;
+    color: #fd0031;
+`;
+const Popli3 = styled.li`
+    font-weight: bold;
+    margin: 20px 0;
+    list-style: none;
+`;
+const PopH3 = styled.h3`
+    font-size: 16px;
+    line-height: 50px;
+    border-bottom: 1px solid #eee;
+`;
+const PopH3Input = styled.input`
+    width: 100px;
+    font-size: 14px;
+    border: none;
+    border-bottom: 1px solid #333;
+    margin-right: 10px;
+    padding: 6px 0;
+    font-weight: bold;
+    text-align: center;
+    outline: none;
+`;
+const Popli4 = styled.li`
+    display: inline-block;
+    width: 50%;
+    margin-left: -2px;
+    list-style: none;
+`;
+const Popli4Button = styled.button`
+    margin-right: 15px;
+    border: 1px solid #333;
+    color: #666;
+    margin-top: 10px;
+    margin-bottom: 5px;
+    width: 43%;
+    background: #fff;
+    font-size: 14px;
+    font-weight: bold;
+    border-radius: 20px;
+    line-height: 25px;
+    outline: none;
+    cursor: pointer;
+`;
+const Popli5 = styled.li`
+    display: inline-block;
+    width: 50%;
+    margin-left: -2px;
+    list-style: none;
+`;
+const Popli5Button = styled.button`
+    margin-top: 10px;
+    margin-bottom: 5px;
+    width: 43%;
+    border: 1px solid #fd0031;
+    background: #fff;
+    font-size: 14px;
+    font-weight: bold;
+    border-radius: 20px;
+    line-height: 25px;
+    color: #fd0031;
+    outline: none;
+    cursor: pointer;
+`;
