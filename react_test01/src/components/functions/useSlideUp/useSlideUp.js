63ca74ab612f8ee1333a74d15a66b715efc97f8d
askePhoenix <aistarteverything@gmail.com>
2021-05-06 18:38:59 +0900

    useEffect, useRef 위치 수정(build error) #40

--- react_test01/src/components/functions/useSlideUp/useSlideUp.js
+++ react_test01/src/components/functions/useSlideUp/useSlideUp.js
@@ -3,16 +3,18 @@ import { useRef, useEffect } from 'react';
 //사용 컴포넌트에 토글 값 변경 state 필요함.
 
 export const useSlideUp = (duration = 1, toggle) => {
-    if (typeof duration !== "number" ) {
-      return;
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `transform ${duration}s ease-in-out 0s`;
+      current.style.transform = "translate(-50%, -100%)";
     }
-    const element = useRef();
-    useEffect(() => {
-      if (element.current) {
-        const { current } = element;
-        current.style.transition = `transform ${duration}s ease-in-out 0s`;
-        current.style.transform = "translate(-50%, -100%)";
-      }
-    },[toggle]);
-    return { ref: element, style: { transform: "translate(-50%, 0)" } };
-  };
+  },[toggle]);
+  
+  if (typeof duration !== "number" ) {
+    return;
+  }
+  
+  return { ref: element, style: { transform: "translate(-50%, 0)" } };
+};
