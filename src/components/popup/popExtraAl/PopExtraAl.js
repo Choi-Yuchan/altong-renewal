082750e47701aac9eec006034d0137da5e236a93
Choi-Yuchan <67131983+Choi-Yuchan@users.noreply.github.com>
2021-06-24 14:53:44 +0900

    Modify a gift member list component (#193)

--- src/components/popup/popExtraAl/PopExtraAl.js
+++ src/components/popup/popExtraAl/PopExtraAl.js
@@ -1,41 +1,28 @@
 import styled from 'styled-components';
-
 import Num3Comma from '../../functions/num3comma/Num3Comma'
 import FormatDateAsText from '../../functions/formatDateAsText/FormatDateAsText'
-
 import {useTranslation} from 'react-i18next';
-
-const ExtraViewer = (nick, money, time) => {
-    return <>
-        <AlmoneyDivUlLi2>
-          <AlmoneyDivUlLi2A>
-              <b>{nick} (<Num3Comma num={money}></Num3Comma>)</b>
-          </AlmoneyDivUlLi2A>
-          <FormatDateAsText date={Date.parse(time)}></FormatDateAsText>
-        </AlmoneyDivUlLi2>
-    </>
-}
-
-const ExtraAlList = (lists) => {
-    return lists.map((list)=>{
-        return ExtraViewer(list.nickname, list.almoney, list.conDate);
-    })
-  }
  
-function PopExtraAl(props) {
-    const {t} = useTranslation();
-    const extraList = props.extraList;
-    const showExtraList= props.showExtraList;
-    
-    return (
-      showExtraList === true &&  <AlmoneyDivUl show={showExtraList}>
-          <AlmoneyDivUlLi>{t('PopExtraAl_Given')}</AlmoneyDivUlLi>
-          {ExtraAlList(extraList)}
-      </AlmoneyDivUl>    
-      );
+function PopExtraAl({showExtraList, extraList}) {
+  const {t} = useTranslation();
+
+  return (
+    showExtraList === true && 
+    <WarmingAlList show={showExtraList}>
+        <GiftMemberTitle>{t('PopExtraAl_Given')}</GiftMemberTitle>
+        {extraList.map(list =>         
+        <GiftMemberList key={extraList.userSeq}>
+          <GiftMember>
+              <b>{list.nickname} (<Num3Comma num={list.almoney}></Num3Comma>)</b>
+          </GiftMember>
+          <FormatDateAsText date={Date.parse(list.conDate)}></FormatDateAsText>
+        </GiftMemberList>
+          )}
+    </WarmingAlList>    
+    );
   }
 
-const AlmoneyDivUl = styled.ul`
+const WarmingAlList = styled.ul`
   display: ${(props) => props.show?"flex":"none"};
   flex-direction: column;
   flex-wrap: nowrap;
@@ -51,16 +38,16 @@ const AlmoneyDivUl = styled.ul`
   z-index: 999;
   box-sizing:border-box;
 `;
-const AlmoneyDivUlLi = styled.li`
+const GiftMemberTitle = styled.li`
   color: #f2056e;
   margin-bottom: 0.625rem;
   list-style: none;
 `;
-const AlmoneyDivUlLi2 = styled.li`
+const GiftMemberList = styled.li`
   line-height: 1.5rem;
   list-style: none;
 `;
-const AlmoneyDivUlLi2A = styled.a`
+const GiftMember = styled.a`
   text-decoration: none;
   color: #333;
   margin-right:0.3125rem;
